b13fd4a5125bb82ce8875127bc3c34ac98f31096
bnbn332 <bnbn332@naver.com>
2022-07-04 23:58:30 +0900

    22-07-04

---
+++ 집합과 맵/1764_듣보잡.py
@@ -0,0 +1,19 @@
+# 듣보잡 (set이용)
+from sys import stdin
+
+n, m = map(int, input().split())     # 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M
+
+listen = set()
+for _ in range(n):
+    listen.add(stdin.readline().strip())
+
+see = set()
+for _ in range(m):
+    see.add(stdin.readline().strip())
+
+print(listen)
+answer = sorted(list(listen & see))
+
+print(len(answer))
+for i in answer:
+    print(i)
