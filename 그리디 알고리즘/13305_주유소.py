a3c425a3f6ef33bdaac3097c4214b47f0f193c06
bnbn332 <bnbn332@naver.com>
2022-07-25 22:34:01 +0900

    22-07-25

---
+++ 그리디 알고리즘/13305_주유소.py
@@ -0,0 +1,20 @@
+# 주유소
+
+# 도시의 개수 N
+n = int(input())
+
+# 도로의 길이 리스트
+road_length = list(map(int, input().split()))
+
+# 주유소 리터당 가격
+oil_price = list(map(int, input().split()))
+
+min_price = oil_price[0]
+sum = 0
+
+for i in range(n - 1):
+    if min_price > oil_price[i]:
+        min_price = oil_price[i]
+    sum += (min_price * road_length[i])
+
+print(sum)
